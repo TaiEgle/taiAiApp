68e315170a991e4a809cdf2aa44622e9e03ca2bc
KF00000121-郭宇豪 <lli_2tt1a8xeu@dingtalk.com>
2026-07-10 10:05:38 +0800

    chore: 更新项目依赖和配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
-import { defineConfig } from '@dcloudio/vite-plugin-uni'
+import uni from '@dcloudio/vite-plugin-uni'
 
-export default defineConfig({
+export default {
+  plugins: [uni()],
   server: {
     port: 3000,
     host: '0.0.0.0'
   }
-})
+}
